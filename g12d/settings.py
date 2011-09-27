9143708dec8f0a938a0ab8efa37bc1b00d13420c
Helmy Giacoman <helmygb@gmail.com>
2011-09-27 14:52:53 -0600

    :D

--- g12d/settings.py
+++ g12d/settings.py
@@ -96,9 +96,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
