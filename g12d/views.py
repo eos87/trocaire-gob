1d0f3572e674446fffc1a3144924d2a9544da7a4
Helmy Giacoman <helmygb@gmail.com>
2011-11-19 16:57:01 -0600

    show orgs

--- g12d/views.py
+++ g12d/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from trocaire.models import *
 
 def home(request):
+    organizaciones = Organizacion.objects.all()
     return render_to_response('index.html', RequestContext(request, locals()))
