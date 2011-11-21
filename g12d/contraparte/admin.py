fb4c3866742e3e11af1aa56e01e4d0be1dd0a5d5
Helmy Giacoman <helmygb@gmail.com>
2011-11-21 14:51:46 -0600

    fix admin.py

--- g12d/contraparte/admin.py
+++ g12d/contraparte/admin.py
@@ -28,7 +28,7 @@ admin.site.register(Organizador)
 class ActividadAdmin(admin.ModelAdmin):
     list_filter = ['nombre_actividad', 'organizacion', 'proyecto', 'persona_organiza', 'fecha']
     search_fields = ['nombre_actividad', 'organizacion__nombre_corto', 'persona_organiza__nombre']
-    list_display = ['nombre_actividad', 'organizacion', 'proyecto', 'fecha', 'resultado']
+    list_display = ['nombre_actividad', 'organizacion__nombre_corto', 'proyecto__codigo', 'fecha', 'resultado__nombre_corto']
     
     fieldsets = [
         (None, {'fields': [('organizacion', 'proyecto'), 'persona_organiza', 'nombre_actividad', 'fecha',
